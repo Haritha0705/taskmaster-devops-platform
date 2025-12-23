6bd893805048d730ea1dfd80314c4c686031abe6
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-12-23 10:39:03 +0530

    Add SwaggerConfig for OpenAPI documentation with JWT security integration

---
+++ src/main/java/com/taskmaster/config/swagger/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package com.taskmaster.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${application.version:1.0.0}")
+    private String appVersion;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library Management System API")
+                        .description("RESTful API for Library Management System - Demo Spring Boot Project")
+                        .version(appVersion)
+                        .contact(new Contact()
+                                .name("Task Master")
+                                .url("https://github.com/Haritha0705/taskmaster-devops-platform"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Development Server"),
+                        new Server().url("https://api.taskmaster.com").description("Production Server")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT Bearer token")));
+    }
+}

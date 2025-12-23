8075a5b441533b3055b888811485ef3d9dad82fd
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-12-23 13:48:11 +0530

    Add RegisterRequest DTO with validation for user registration input

---
+++ src/main/java/com/taskmaster/feature/auth/dto/request/RegisterRequest.java
@@ -0,0 +1,42 @@
+package com.taskmaster.feature.auth.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
+            message = "Password must contain at least one digit, one lowercase, one uppercase, and one special character"
+    )
+    private String password;
+
+    @NotBlank(message = "Password confirmation is required")
+    private String confirmPassword;
+
+    @NotBlank(message = "Role is required")
+    private String role;
+
+    private String phone;
+}
